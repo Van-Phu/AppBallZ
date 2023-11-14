817cf952f0a7d0135d2f07a08da050e82656cbe3
vanduongg <103979655+vanduongg@users.noreply.github.com>
2023-11-14 14:19:13 +0700

    Merge branch 'master' of D:\hello1\VangLangUNI\Nam3\AndroidProject_GitKraken\AppBallZ with conflicts.

--- app/src/main/java/com/example/ballz/MainActivity.java
+++ app/src/main/java/com/example/ballz/MainActivity.java
@@ -1,28 +1,14 @@
 package com.example.ballz;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private ActionBar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = getSupportActionBar();
-
-        toolbar.setTitle("Shop");
-        loadFragment(new MainFrag());
     }
-    private void loadFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragMain, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
-
 }
