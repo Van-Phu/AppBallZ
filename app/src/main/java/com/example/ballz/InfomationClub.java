3c40ae38fa132fe3b1adbd2e2baebb51294e9eb1
vanduongg <103979655+vanduongg@users.noreply.github.com>
2023-12-07 00:57:34 +0700

    add infoClub
    design infoClub

---
+++ app/src/main/java/com/example/ballz/InfomationClub.java
@@ -0,0 +1,14 @@
+package com.example.ballz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfomationClub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infomation_club);
+    }
+}
